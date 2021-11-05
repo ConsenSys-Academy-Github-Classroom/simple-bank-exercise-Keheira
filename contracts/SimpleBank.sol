79dad8a6b211bfe80fdd520d88b33f087b103c63
Keheira <keheirah@gmail.com>
2021-11-05 02:47:46 -0500

    revert fallback funtion

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -33,7 +33,7 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    fallback () external payable {
+    function () external payable {
         revert();
     }
 
